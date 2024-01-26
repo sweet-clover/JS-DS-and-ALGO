70679c5699cb72afad006e078f43ed24a0def720
Sweet Clover <ms.greenclover13@gmail.com>
2024-01-26 13:51:20 +0630

    add hash table

---
+++ 10_hash-table/code.js
@@ -0,0 +1,32 @@
+function hashCode(key){
+    let hash = 0
+    for(let i = 0; i < key.length; i++){
+        hash += key.charCodeAt(i)
+    }
+    return hash % 37
+}
+
+class HashTable{
+    constructor(){
+        this.table = []
+    }
+
+    put(key, value){
+        let index = hashCode(key)
+        this.table[index] = value
+    }
+
+    get(key){
+        let index = hashCode(key)
+        return this.table[index]
+    }
+
+    remove(key){
+        let index = hashCode(key)
+        delete this.table[index]
+    }
+}
+
+let ht = new HashTable()
+ht.put("Coverl", "A clover is a small plant with bright green leaves")
+console.log("table", ht.get("Clover"))
